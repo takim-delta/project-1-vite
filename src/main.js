35fb76654c02fa254ebea053865629a5ce575ae2
Çağdaş Şahin <3cagdas3@gmail.com>
2025-03-26 15:06:06 +0300

    some changes.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 //----modalBtn----//
-let modalBtn = document.querySelector('.header-btn-menu');
-let modalBtnClose = document.querySelector('.header-btn-close');
-let modal = document.querySelector('.mobile-menu');
+const modalBtn = document.querySelector('.header-btn-menu');
+const modalBtnClose = document.querySelector('.mobile-btn-close');
+const modal = document.querySelector('.mobile-menu');
 
 //----modalBtn----//
 
 modalBtn.addEventListener('click', function () {
-    modalBtn.classList.toggle('active');
-    modal.classList.toggle('active');
+  modalBtn.classList.toggle('active');
+  modal.classList.toggle('active');
 });
 
 modalBtnClose.addEventListener('click', function () {
-    modalBtn.classList.toggle('active');
-    modal.classList.toggle('active');
+  modalBtn.classList.toggle('active');
+  modal.classList.toggle('active');
 });
 
 //----modalBtnLinks----//
